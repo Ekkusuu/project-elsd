70b8c6f9434bf0fd05d9ee072c7422c80c1cebcf
Mihai Gurduza <mihai.gurduza@isa.utm.md>
2025-05-18 00:00:43 +0300

    add models to package init file

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,6 @@ from .TimelineInterpreter import TimelineInterpreter
 from .TimelineParser import TimelineParser
 from .TimelineLexer import TimelineLexer
 from .TimelineParserVisitor import TimelineParserVisitor
+from .models import *
 
-__all__ = ["TimelineParser", "TimelineLexer", "TimelineInterpreter", "TimelineParserVisitor"]
+__all__ = ["TimelineParser", "TimelineLexer", "TimelineInterpreter", "TimelineParserVisitor", "Event", "Period", "Timeline", "Relationship"]
